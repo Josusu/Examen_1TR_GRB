76bd9b457afd6a1f4e4926e9cca717eee977c8b7
Josué Rodríguez Cuétara <josuerc00@gmail.com>
2020-12-09 17:50:18 +0100

    Añado materiales
    He añadido materiales a la nave, plano y esfera

---
+++ examen/Assets/Scripts/instanciadorEsferas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciadorEsferas : MonoBehaviour
+{
+    //Variable para asociar esfera
+    public GameObject Esfera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
